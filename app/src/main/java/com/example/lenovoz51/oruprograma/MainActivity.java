0c446b0dacc7df6f26e7e9c631576f336196d654
arnnak <arnasnak@gmail.com>
2018-10-07 16:31:05 +0300

    JsonObject

--- app/src/main/java/com/example/lenovoz51/oruprograma/MainActivity.java
+++ app/src/main/java/com/example/lenovoz51/oruprograma/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.lenovoz51.oruprograma;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -34,5 +35,35 @@ public class MainActivity extends AppCompatActivity {
         temp = (TextView)findViewById(R.id.temperatura);
         dec = (TextView)findViewById(R.id.aprasymas);
         updated = (TextView)findViewById(R.id.atnaujinimas);
+
+        gavimas();
+
+
+    }
+
+    private void gavimas(){
+        WeatherTask weatherTask = new WeatherTask();
+        weatherTask.execute(Utils.BASE_URL);
+    }
+
+    private class WeatherTask extends AsyncTask<String, Void, Weather>{
+
+        @Override
+        protected Weather doInBackground(String... strings) {
+            String data = (new WeatherHttpClient()).getWeatherData(Utils.BASE_URL);
+            Weather weather = JsonWeatherParser.getWeather(data);
+            return weather;
+        }
+
+        @Override
+        protected void onPostExecute(Weather weather){
+            super.onPostExecute(weather);
+            /*cityname.setText(weather.location.getCity());
+            humidity.setText((int) weather.currentCondition.getHumidity());
+            temp.setText((int) weather.temperature.getTemp());
+            dec.setText(weather.currentCondition.getDescription());
+            sunset.setText((int) weather.location.getSunset());
+            sunrise.setText((int) weather.location.getSunrise());*/
+        }
     }
 }
--- app/src/main/java/com/example/lenovoz51/oruprograma/WeatherHttpClient.java
+++ app/src/main/java/com/example/lenovoz51/oruprograma/WeatherHttpClient.java
@@ -1,5 +1,7 @@
 package com.example.lenovoz51.oruprograma;
 
+import android.util.Log;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -17,7 +19,7 @@ public class WeatherHttpClient {
         HttpURLConnection connection = null;
         InputStream inputStream = null;
         try {
-            connection = (HttpURLConnection) (new URL(Utils.BASE_URL + place)).openConnection();
+            connection = (HttpURLConnection) (new URL(Utils.BASE_URL)).openConnection();
             connection.setRequestMethod("GET");
             connection.setDoInput(true);
             connection.setDoOutput(true);
@@ -29,7 +31,6 @@ public class WeatherHttpClient {
             String line = null;
             while((line = bufferedReader.readLine()) != null){
                 stringBuffer.append(line+"\r\n");
-
             }
 
             inputStream.close();
